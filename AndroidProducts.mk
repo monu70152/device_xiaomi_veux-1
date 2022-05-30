8f3f433e06556782b31ae348bfe18ba696a38de2
JaswalAshish <jaswalashish48@gmail.com>
2022-05-30 19:58:02 +0530

    veux: Its Snow time
    Signed-off-by: JaswalAshish <jaswalashish48@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_veux.mk
+    $(LOCAL_DIR)/evolution_veux.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_veux-user \
-    aosp_veux-userdebug \
-    aosp_veux-eng
+    evolution_veux-user \
+    evolution_veux-userdebug \
+    evolution_veux-eng
--- evolution_veux.mk
+++ evolution_veux.mk
@@ -8,19 +8,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-TARGET_USES_AOSP_RECOVERY := true
+# Inherit from veux device
+$(call inherit-product, device/xiaomi/veux/device.mk)
+
+# Inherit some common Evolution-X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
 
-# Inherit from veux device
-$(call inherit-product, device/xiaomi/veux/device.mk)
-
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-PRODUCT_NAME := aosp_veux
+PRODUCT_NAME := evolution_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
